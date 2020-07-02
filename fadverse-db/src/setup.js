3da1f4910fe81e99a5fc6ee51a72d90eca846e4d
Fadith Escorcia <feescorciam@protonmail.com>
2020-07-02 00:19:40 -0500

    inicio de configuración de mqtt

--- fadverse-db/src/setup.js
+++ fadverse-db/src/setup.js
@@ -6,16 +6,22 @@ const chalk = require('chalk')
 const db = require('./')
 
 const prompt = inquirer.createPromptModule()
+const args = process.argv.slice()
 
-async function setup () {
-  const answer = await prompt([{
-    type: 'confirm',
-    name: 'setup',
-    message: 'This will destroy all database, are you sure?'
-  }])
-  if (!answer.setup) {
-    return console.log('Nothing happened ;)')
+async function setup() {
+  if (!args.includes('-y') && !args.includes('--y')) {
+    const answer = await prompt([
+      {
+        type: 'confirm',
+        name: 'setup',
+        message: 'This will destroy all database, are you sure?',
+      },
+    ])
+    if (!answer.setup) {
+      return console.log('Nothing happened ;)')
+    }
   }
+
   const config = {
     database: process.env.DB_NAME || 'fadverse',
     username: process.env.DB_USER || 'admin',
@@ -23,14 +29,14 @@ async function setup () {
     host: process.env.DB_HOST || 'localhost',
     dialect: 'postgres',
     logging: (s) => debug(s),
-    setup: true
+    setup: true,
   }
   await db(config).catch(handleFatalError)
   console.log('Success!')
   process.exit(0)
 }
 
-function handleFatalError (err) {
+function handleFatalError(err) {
   console.error(`${chalk.red('[fatal error]')} ${err.message}`)
   console.error(err.stack)
   process.exit(1)
