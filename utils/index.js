a087cc1311d6fdb72c28e4e87e8a793043ec3dfc
Fadith Escorcia <feescorciam@protonmail.com>
2020-07-12 23:46:17 -0500

    todo

--- utils/index.js
+++ utils/index.js
@@ -1,7 +1,18 @@
 'use strict'
 
-const config = require('./tools/config')
+const debug = require('debug')('fadverse:db:config')
 
 module.exports = {
-  config
+  db: {
+    database: process.env.DB_NAME || 'fadverse',
+    username: process.env.DB_USER || 'admin',
+    password: process.env.DB_PASS || 'arianna',
+    host: process.env.DB_HOST || 'localhost',
+    dialect: 'postgres',
+    logging: (s) => debug(s),
+  },
+  auth: {
+    secret: process.env.SECRET || 'platzi',
+    algorithms: ['HS256']
+  },
 }
